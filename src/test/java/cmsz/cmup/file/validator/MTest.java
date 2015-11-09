5207109b286a18be20092cdf4ddf38c06b05ceff
shuangzh <shuangzh@qq.com>
2015-11-09 20:01:40 +0800

    增加类

---
+++ src/test/java/cmsz/cmup/file/validator/MTest.java
@@ -0,0 +1,38 @@
+package cmsz.cmup.file.validator;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import cmsz.cmup.file.validator.impl.ValidatorImpl;
+
+public class MTest {
+
+	public static void main(String[] args) {
+		
+		ValidatorImpl v = new ValidatorImpl();
+		
+		v.setRegxFile("reg.txt");
+		
+		try {
+			v.prepare();
+			v.validate(new FileInputStream("sample_1.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ValidateException e) {
+			List<ValidateException> l=e.trace();
+			for(ValidateException v2: l){
+				System.out.println(v2.toString());
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
